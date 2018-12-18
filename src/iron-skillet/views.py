35070ad62ca692ffb32b58a0b8df1a15344868b3
Nathan <nembery@gmail.com>
2018-12-18 17:21:14 -0500

    add workflow example

--- src/iron-skillet/views.py
+++ src/iron-skillet/views.py
@@ -21,3 +21,40 @@ class DockerTestView(CNCBaseFormView):
         context['results'] = r
         return render(self.request, 'pan_cnc/results.html', context)
 
+
+class IronSkilletWorkflow01(CNCBaseFormView):
+
+    def generate_dynamic_form(self):
+        self.fields_to_render = ['PANORAMA_TYPE', 'MGMT_TYPE']
+        return super().generate_dynamic_form()
+
+    def form_valid(self, form):
+        panorama_type = self.get_value_from_workflow('PANORAMA_TYPE', '')
+        mgmt_type = self.get_value_from_workflow('MGMT_TYPE', '')
+
+        if panorama_type == 'static' or mgmt_type == 'static':
+            print('we need some info from the user')
+            self.next_url = 'workflow02'
+        else:
+            print('no info needed')
+            self.next_url = 'workflow03'
+
+        return super().form_valid(form)
+
+
+class IronSkilletWorkflow02(CNCBaseFormView):
+
+    def generate_dynamic_form(self):
+        panorama_type = self.get_value_from_workflow('PANORAMA_TYPE', '')
+        print(panorama_type)
+        if panorama_type == 'static':
+            print('adding panorama static fields')
+            self.fields_to_render += ['PANORAMA_IP', 'PANORAMA_MASK']
+
+        mgmt_type = self.get_value_from_workflow('MGMT_TYPE', '')
+        print(mgmt_type)
+        if mgmt_type == 'static':
+            print('adding mgmt static fields')
+            self.fields_to_render += ['MGMT_IP', 'MGMT_MASK']
+
+        return super().generate_dynamic_form()
